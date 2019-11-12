1b24ae1e54e53afc8f927be31be02fefd18f9788
eybbus <eythorfreyr94@gmail.com>
2019-11-12 13:15:25 +0000

    Removed unused environment variable

--- config.js
+++ config.js
@@ -2,7 +2,6 @@ module.exports = {
   prefix: 'botus ',
   discordToken: process.env.DISCORD_TOKEN,
   youtubeKey: process.env.YOUTUBE_KEY,
-  discord: process.env.DISCORD_KEY,
   soundCloudKey: process.env.SOUNDCLOUD_ID,
   owner: process.env.OWNER_ID,
   volumeAmount: 0.2
