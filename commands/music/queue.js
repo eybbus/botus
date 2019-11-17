9c4b363f2676d586fbf48cdc8dcb6bc435445596
eybbus <eythorfreyr94@gmail.com>
2019-11-17 01:50:39 +0000

    Queue command refined to support the new structure of server queue

--- commands/music/queue.js
+++ commands/music/queue.js
@@ -1,33 +1,45 @@
-const { Command } = require('discord.js-commando');
-const { RichEmbed } = require('discord.js');
-const music = require('./play');
+const { Command } = require("discord.js-commando");
+const { RichEmbed } = require("discord.js");
+const { music } = require("../../bot");
 
 module.exports = class QueueCommand extends Command {
   constructor(client) {
     super(client, {
-      name: 'queue',
-      aliases: ['song-list', 'next-songs'],
-      group: 'music',
-      memberName: 'queue',
-      description: 'Display the song queue'
+      name: "queue",
+      aliases: ["song-list", "next-songs"],
+      group: "music",
+      memberName: "queue",
+      description: "Display the song queue"
     });
   }
 
-  run(message) {
-    const queue = music.queue;
-    if (!queue) return message.say('There are no songs in queue!');
-    const titleArray = [];
-    queue.map(obj => {
-      titleArray.push(obj.title);
+  run(msg) {
+    const queue = music.getQueue(msg.guild.id);
+    if (!queue.songs.length) return msg.say("There are no songs in queue!");
+    let nowPlaying = queue.currentSong;
+    let que = ``;
+    for (var i = 0; i < queue.songs.length; i++) {
+      if (i > 9) {
+        que += `\n out of ${queue.songs.length} songs`;
+        break;
+      }
+      que += `\n${i + 1}. **${queue.songs[i].title}**\nRequested By: ${
+        queue.songs[i].requestedBy
+      }`;
+    }
+    let resp = [
+      { name: `Now Playing`, value: `**${nowPlaying.title}**` },
+      { name: `Requested By`, value: nowPlaying.requestedBy },
+      { name: `Queue [${queue.songs.length} songs]`, value: que }
+    ];
+
+    msg.channel.send({
+      embed: {
+        title: "Music Queue",
+        fields: resp
+      }
     });
 
-    var queueEmbed = new RichEmbed()
-      .setColor('#ff7373')
-      .setTitle('Music Queue');
-    for (let i = 0; i < titleArray.length; i++) {
-      queueEmbed.addField(`${i + 1}: ${titleArray[i]}`);
-    }
-
-    return message.say(queueEmbed);
+    return;
   }
 };
