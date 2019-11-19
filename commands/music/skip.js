abd52b4ca346088dbcaf603ecb7d886365b56158
eybbus <eythorfreyr94@gmail.com>
2019-11-19 11:17:40 +0000

    Added a skip message

--- commands/music/skip.js
+++ commands/music/skip.js
@@ -1,28 +1,29 @@
-const { Command } = require('discord.js-commando');
-const music = require('./play');
+const { Command } = require("discord.js-commando");
+const music = require("./play");
 
 module.exports = class SkipCommand extends Command {
   constructor(client) {
     super(client, {
-      name: 'skip',
-      group: 'music',
-      memberName: 'skip',
-      description: 'Skips the current playing song',
-      examples: ['skip']
+      name: "skip",
+      group: "music",
+      memberName: "skip",
+      description: "Skips the current playing song",
+      examples: ["skip"]
     });
   }
 
   run(msg) {
     if (!msg.member.voiceChannel) {
-      return msg.reply('Must be in a channel to skip');
+      return msg.reply("Must be in a channel to skip");
     }
 
     const dispathcer = music.dispatcher;
 
-    if (typeof dispathcer == 'undefined') {
-      return msg.say('No song is playing');
+    if (typeof dispathcer == "undefined") {
+      return msg.say("No song is playing");
     }
 
     dispathcer.end();
+    msg.reply("Song Skipped");
   }
 };
