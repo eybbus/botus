69eec22850180a8b264e3b62cd9022f27b28e765
eybbus <eythorfreyr94@gmail.com>
2019-11-19 11:20:16 +0000

    disconnect and resume made to work with the new music class.

--- commands/music/disconnect.js
+++ commands/music/disconnect.js
@@ -1,27 +1,28 @@
-const { Command } = require('discord.js-commando');
-const music = require('./play');
+const { Command } = require("discord.js-commando");
+const music = require("./play");
 
 module.exports = class QueueCommand extends Command {
   constructor(client) {
     super(client, {
-      name: 'disconnect',
-      aliases: ['dc', 'abort', 'abortus', 'leave'],
-      group: 'music',
-      memberName: 'disconnect',
-      description: 'Disconnect from the voiceChannel'
+      name: "disconnect",
+      aliases: ["dc", "abort", "abortus", "leave"],
+      group: "music",
+      memberName: "disconnect",
+      description: "Disconnect from the voiceChannel"
     });
   }
 
   run(msg) {
-    const { dispatcher, queue } = music;
+    const { dispatcher } = music;
     if (!msg.guild.voiceConnection) {
       return msg.say("I'm not in a voice channel right now");
     } else if (msg.guild.voiceConnection) {
-      if (typeof dispathcer == 'undefined') {
+      if (typeof dispatcher == "undefined") {
         return msg.guild.voiceConnection.disconnect();
       }
-      queue.length = 0;
-      return dispatcher.end();
-		}
+      dispatcher.end();
+      msg.guild.voiceConnection.disconnect();
+      msg.reply("disconnected");
+    }
   }
 };
--- commands/music/resume.js
+++ commands/music/resume.js
@@ -1,28 +1,41 @@
-const { Command } = require('discord.js-commando');
-const music = require('./play');
-
+const { Command } = require("discord.js-commando");
+const playCommand = require("./play");
+const { music } = require("../../bot");
 module.exports = class ResumeCommand extends Command {
   constructor(client) {
     super(client, {
-      name: 'resume',
-      group: 'music',
-      memberName: 'resume',
-      description: 'Resumes the current playing song',
-      examples: ['resume']
+      name: "resume",
+      group: "music",
+      memberName: "resume",
+      description: "Resumes the current playing song",
+      examples: ["resume"]
     });
   }
 
   run(msg) {
-    if (!msg.member.voiceChannel) {
-      return msg.reply('Must be in a channel to pause');
+    const dispatcher = playCommand.dispatcher;
+    if (typeof dispatcher == "undefined") {
+      return msg.say("No current song");
+    } else if (!dispatcher.paused) {
+      return msg.say("No song paused");
     }
 
-    const dispatcher = music.dispatcher;
+    const queue = music.getQueue(msg.guild.id);
 
-    if (typeof dispatcher == 'undefined') {
-      return msg.say('No song is playing');
+    if (msg.member.voiceChannel) {
+      if (Object.keys(queue.currentSong).length === 0) {
+        return msg.say("No current song");
+      }
+      if (msg.guild.voiceConnection) {
+        dispatcher.resume();
+        msg.say("resumed");
+      } else {
+        //TODO: call the playSong funtion instead of dispatcher.resume();
+        queue.currentSong.voiceChannel.join().then(connection => {
+          dispatcher.resume();
+          msg.say("connected and resumed");
+        });
+      }
     }
-
-    dispatcher.resume();
   }
 };
